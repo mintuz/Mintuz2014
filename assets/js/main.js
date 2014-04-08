597bbbd361fc2b6af9af8ea8330ff79ca26e91c1
Adam Bulmer <mintuz1990@gmail.com>
2014-04-08 21:31:25 +0100

    Form Styles / Bug Fixes & Single Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,37 +47,41 @@ var AMBFrontend = {
 	},
 	frontpage_charts : function() {
 		
-		var ctx = $( "#gaming-chart" ).get( 0 ).getContext( "2d" );
+		if( $( ".chart" ).length > 0 ) {
 
-		new Chart(ctx).Radar({
-			
-			labels : [ "","Call Of Duty","Battlefield","Guild Wars 2","Minecraft","Diablo 3" ],
-			datasets : [
-				{
-					fillColor : "rgba(56,64,71,0.5)",
-					strokeColor : "rgba(43,49,54,0.5)",
-					data : [1,100,60,70,90,40]
-				}
-			]
+			var ctx = $( "#gaming-chart" ).get( 0 ).getContext( "2d" );
 
-		});
+			new Chart(ctx).Radar({
+				
+				labels : [ "","Call Of Duty","Battlefield","Guild Wars 2","Minecraft","Diablo 3" ],
+				datasets : [
+					{
+						fillColor : "rgba(56,64,71,0.5)",
+						strokeColor : "rgba(43,49,54,0.5)",
+						data : [1,100,60,70,90,40]
+					}
+				]
+
+			});
 
 
 
-		var ctx = $( "#dev-chart" ).get( 0 ).getContext( "2d" );
+			var ctx = $( "#dev-chart" ).get( 0 ).getContext( "2d" );
 
-		new Chart(ctx).Radar({
-			
-			labels : [ "Frontend Web","Backend Web","Wordpress","Laravel","Objective C" ],
-			datasets : [
-				{
-					fillColor : "rgba(83,187,180,0.5)",
-					strokeColor : "rgba(53,135,130,0.5)",
-					data : [100,70,80,40,60,1]
-				}
-			]
+			new Chart(ctx).Radar({
+				
+				labels : [ "Frontend Web","Backend Web","Wordpress","Laravel","Objective C" ],
+				datasets : [
+					{
+						fillColor : "rgba(83,187,180,0.5)",
+						strokeColor : "rgba(53,135,130,0.5)",
+						data : [100,70,80,40,60,1]
+					}
+				]
 
-		});
+			});
+
+		}
 
 
 	}
